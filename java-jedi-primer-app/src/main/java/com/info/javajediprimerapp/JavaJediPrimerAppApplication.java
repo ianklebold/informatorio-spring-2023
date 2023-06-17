320afe635e49aceea72ec1dd5a5185bbf895edd3
ianklebold <ianstgo@gmail.com>
2023-06-17 11:31:59 -0300

    New project

---
+++ java-jedi-primer-app/src/main/java/com/info/javajediprimerapp/JavaJediPrimerAppApplication.java
@@ -0,0 +1,13 @@
+package com.info.javajediprimerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJediPrimerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJediPrimerAppApplication.class, args);
+	}
+
+}
---
+++ java-jedi-primer-app/src/test/java/com/info/javajediprimerapp/JavaJediPrimerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.javajediprimerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJediPrimerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
