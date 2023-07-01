10a7c351cc014d2964f3ee8bd2ffa7b5e8d1943a
ianklebold <ianstgo@gmail.com>
2023-07-01 20:32:37 -0300

    BOOK 004 - Create y Read con repository

--- java-jedi-primer-app/src/main/java/com/info/javajediprimerapp/repository/book/BookRepository.java
+++ java-jedi-primer-app/src/main/java/com/info/javajediprimerapp/repository/book/BookRepository.java
@@ -1,2 +1,14 @@
-package com.info.javajediprimerapp.repository.book;public interface BookRepository {
+package com.info.javajediprimerapp.repository.book;
+
+import com.info.javajediprimerapp.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {
+    //Query Methods
+    Optional<Book> findBookByTitleEqualsIgnoreCase(String title);
 }
--- java-jedi-primer-app/src/main/java/com/info/javajediprimerapp/service/impl/BookServiceJPAImpl.java
+++ java-jedi-primer-app/src/main/java/com/info/javajediprimerapp/service/impl/BookServiceJPAImpl.java
@@ -1,2 +1,69 @@
-package com.info.javajediprimerapp.service.impl;public class BookServiceJPAImpl {
+package com.info.javajediprimerapp.service.impl;
+
+import com.info.javajediprimerapp.domain.Book;
+import com.info.javajediprimerapp.repository.book.BookRepository;
+import com.info.javajediprimerapp.service.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Primary
+@Service
+@AllArgsConstructor
+public class BookServiceJPAImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll(); //Traer todos los libros
+    }
+
+    @Override
+    public Book createBook(Book book) {
+        book.setUuid(UUID.randomUUID());
+        bookRepository.save(book); //Guardar en base de datos
+        return book;
+    }
+
+    @Override
+    public Optional<Book> updateBook(UUID uuidBook, Book bookUpdated) {
+        //Buscar libro por ID
+        Optional<Book> bookOptional = bookRepository.findById(uuidBook);
+
+        if(bookOptional.isPresent()){
+            updatingBook(bookOptional.get(),bookUpdated);
+            //Save --> Si existe entonces lo actualiza y sino lo crea.
+            bookRepository.saveAndFlush(bookOptional.get());
+            return bookOptional;
+        }else {
+            return Optional.empty();
+        }
+    }
+
+    private void updatingBook(Book book,Book bookUpdated){
+
+        if (bookUpdated.getTitle() != null){
+            book.setTitle(bookUpdated.getTitle());
+        }
+
+        if (bookUpdated.getAuthor() != null){
+            book.setAuthor(bookUpdated.getAuthor());
+        }
+
+    }
+
+    private boolean deleteBookByName(String title) {
+        Optional<Book> bookOptional = bookRepository.findBookByTitleEqualsIgnoreCase(title);
+        if (bookOptional.isPresent()){
+            bookRepository.delete(bookOptional.get());
+            return true;
+        }
+        return false;
+    }
+
 }
